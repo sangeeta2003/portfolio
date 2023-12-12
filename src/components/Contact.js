7aa084e290be07a6462423f02820403662a35f2e
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-12 12:37:14 +0530

    contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,28 +10,6 @@ export default function Contact(){
      <span style={{ color: '#d90429' }}> Me </span>
      </h1>
      <p className='text-2xl text-white my-2'>If you have nay questions or would like to collaborate,please don't <br></br> hesitate to contact me </p>
-    {/* <a className="text-white"
-    href="mailto:mishrasangeeta944@gmail.com"  
-    target="_blank"             
-    rel="noopener noreferrer"   
-  >
-    <SendIcon/> 
-      mishrasangeeta944@gmail.com
-    
-  </a>
-  <a className="text-white "
-    href="https://www.google.com/maps/place/Bhawanipatna,+Odisha,+India"  
-    target="_blank"             
-    rel="noopener noreferrer"   
-  >
-    <LocationOnIcon/> 
-      Bhawanipatna,odisha,India
-    
-  </a> */}
-  import SendIcon from '@mui/icons-material/Send';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-
-
 
 <div className="flex flex-col items-start px-10">
   <a
@@ -43,7 +21,6 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
     <SendIcon className="mr-2 text-yellow-300" />
     mishrasangeeta944@gmail.com
   </a>
-
   <a
     className="text-white flex items-center py-3"
     href="https://www.google.com/maps/place/Bhawanipatna,+Odisha,+India"
@@ -54,8 +31,14 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
     Bhawanipatna, Odisha, India
   </a>
 </div>
-
-
+     </div>
+            <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-8' >
+                <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-7'>
+                <label className="text-white px-10 text-2xl absolute right-30 top-3 font-bold py-9  
+                mb-6 ">Name</label>
+                <input 
+                className='w-1/2  bg-white text-black px-6 py-2 rounded-md absolute right-16 top-11 lg:mt-0 hover:text-slate-800' placeholder='enter your fullname here'></input>
+                </div>      
             </div>
             <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-8' >
                 <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-7'>
@@ -63,21 +46,7 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
                 mb-6 ">Name</label>
                 <input 
                 className='w-1/2  bg-white text-black px-6 py-2 rounded-md absolute right-16 top-11 lg:mt-0 hover:text-slate-800' placeholder='enter your fullname here'></input>
-                </div>
-
-                
-                
-            </div>
-            <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-8' >
-                <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-7'>
-                <label className="text-white px-10 text-2xl absolute right-30 top-3 font-bold py-9  
-                mb-6 ">Name</label>
-                <input 
-                className='w-1/2  bg-white text-black px-6 py-2 rounded-md absolute right-16 top-11 lg:mt-0 hover:text-slate-800' placeholder='enter your fullname here'></input>
-                </div>
-
-                
-                
+                </div>       
             </div>
             <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-8 py-14   ' >
                 <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-7'>
@@ -85,10 +54,7 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
                 mb-6 ">Email</label>
                 <input 
                 className='w-1/2  bg-white text-black px-6 py-2 rounded-md absolute right-16 top-11 lg:mt-0 hover:text-slate-800' placeholder='enter your email address here'></input>
-                </div>
-
-                
-                
+                </div>     
             </div>
             <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-8 py-28   ' >
                 <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-7'>
@@ -97,27 +63,14 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
                 <input 
                 className='w-1/2  bg-white text-black px-6 py-2 rounded-md absolute right-16 top-11 lg:mt-0 hover:text-slate-800' placeholder='enter your message  here'></input>
                 </div>
-
-                
                 <div className=' h-3/4 w-3/5 absolute bottom-1 right-1 ml-10 py-14  px-9 ' >
                 <div className='  w-1/2 py-6 pl-0 mt-2 absolute right-12 px-9'>
                 <button type="text" className="bg-yellow-500 text-black px-5 py-1 rounded-md mt-4 lg:mt-0 hover:text-slate-800">
                  Send Message
               </button>
-                </div>
-
-                
-                
+                </div> 
+            </div>    
             </div>
-            
-            
-
-                
-
-                
-                
-            </div>
-
             </div>
     )
 } 
